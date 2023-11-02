228de51bb36945a40bd48db24ddffd09d68593f0
Robert J. Kratz [MAC] <robert.kratz03@gmail.com>
2023-11-02 14:19:06 +0100

    added fallback in case .env props are not set

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -6,15 +6,15 @@ const nextConfig = {
         defaultLocale: 'en',
     },
     publicRuntimeConfig: {
-        RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
-        GA_TAG: process.env.GA_TAG,
-        PAGE_NAME: process.env.PAGE_NAME,
+        RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY || '',
+        GA_TAG: process.env.GA_TAG || '',
+        PAGE_NAME: process.env.PAGE_NAME || '',
     },
     serverRuntimeConfig: {
-        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
-        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-        CONTACT_EMAIL: process.env.CONTACT_EMAIL,
-        CONTACT_NAME: process.env.CONTACT_NAME,
+        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY || '',
+        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || '',
+        CONTACT_EMAIL: process.env.CONTACT_EMAIL || '',
+        CONTACT_NAME: process.env.CONTACT_NAME || '',
         EMAIL_TEMPLATES: {
             customer_contact: {
                 de: 'd-06a4edee3c5e40a1972a7fb2d3404874',
